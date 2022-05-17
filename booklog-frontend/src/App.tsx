41f2b3a91cfcca06330b50953b70b26d814a30f9
Hikaru Inomata <inomatahi@icloud.com>
2022-05-17 13:49:56 +0000

    一覧のAPIを叩いて表示させるところまで完了

--- booklog-frontend/src/App.tsx
+++ booklog-frontend/src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import axios from "axios";
 
-function App() {
+type Book = {
+  id: number,
+  title: string,
+  author: string,
+  isbn: string
+}
+
+export const App = () => {
+  const[books, setBooks] = useState<Book[]>([]);
+
+  useEffect(() => {
+    axios.get<Book[]>("http://127.0.0.1:3000/dev/book").then((res) => {
+      setBooks(res.data);
+    })
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {books.map(book => (
+        <p>ID: {book.id} Title: {book.title} author: {book.author} isbn: {book.isbn}</p>
+      ))}
     </div>
   );
 }
-
-export default App;
